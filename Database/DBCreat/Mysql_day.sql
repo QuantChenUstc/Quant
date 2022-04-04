f378ce5ca42d4fd79b67b8f6e91059096089a0ff
Yoga-bf <cbf98012@mail.ustc.edu.cn>
2022-04-05 00:00:51 +0800

    creat MySQL_day_table

--- Database/DBCreat/Mysql_day.sql
+++ Database/DBCreat/Mysql_day.sql
@@ -1,1 +1,33 @@
 drop database if exists data_day;
+
+create database data_day;
+
+use data_day;
+
+create table day(
+    code char(11) not null,
+    code_date DATE not null,
+    display_name char(4) not null,
+    abridge_name char(4) not null,
+    open_price decimal(6,2) not null,
+    close_price decimal(6,2) not null,
+    high_price decimal(6,2) not null,
+    low_price decimal(6,2) not null,
+    pre_close decimal(6,2) not null,
+    volume INT not null,
+    allmoney INT not null,
+    change_ratio decimal(4,2) not null,
+    change_price decimal(6,2) not null,
+    high_limit decimal(6,2) not null,
+    low_limit decimal(6,2) not null,
+    paused TINYINT not null,
+    is_st TINYINT not null,
+    volume_ratio decimal(6,2) not null,
+    ma5 decimal(6,2) not null,
+    ma10 decimal(6,2) not null,
+    ma20 decimal(6,2) not null,
+    ma30 decimal(6,2) not null,
+    ma60 decimal(6,2) not null,
+    ma180 decimal(6,2) not null,
+    primary key(code, code_date)
+);
